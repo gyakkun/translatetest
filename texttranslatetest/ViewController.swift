a47803f0747d9b40ce462ef6a4b7f9a1ca41737b
gyakkun <xiaowufeixia@gmail.com>
2020-10-20 15:47:33 +0800

    Initial Commit

---
+++ texttranslatetest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  texttranslatetest
+//
+//  Created by gyakkun on 2020/10/20.
+//  Copyright © 2020 gyakkun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
