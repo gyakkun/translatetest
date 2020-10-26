d2173c70b253f8ace22f09c28bbcd7186995b492
gyakkun <xiaowufeixia@gmail.com>
2020-10-26 13:57:20 +0800

    Add listener.

--- texttranslatetest/ViewController.swift
+++ texttranslatetest/ViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textBeforetranslated: UITextField!
+    
+    @IBOutlet weak var textTranslated: UITextField!
+    
+    @IBAction func onTextEditChanged(_ sender: UITextField) {
+        
+        var tmpStr = sender.text
+     
+        tmpStr = tmpStr?.split(separator: " ")
+            .map({$0.replacingOccurrences(of: "^.*$", with: "🍕",options: [.regularExpression])})
+            .joined(separator: " ")
+        
+        textTranslated.text = tmpStr
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //textInput.addTarget(self, action: "textFieldDidChange:", forControlEvents: UIControl.Event.EditingChanged)
+   
         // Do any additional setup after loading the view.
     }
-
 
 }
 
